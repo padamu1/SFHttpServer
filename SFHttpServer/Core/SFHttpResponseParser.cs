5b4ce0d9a8eee71d8e5f9621ee3e46be0907d364
Mywatt <padamu1@naver.com>
2025-02-04 01:06:16 +0900

    Set cache setting

--- SFHttpServer/Core/SFHttpResponseParser.cs
+++ SFHttpServer/Core/SFHttpResponseParser.cs
@@ -25,9 +25,14 @@ namespace SFHttpServer.Core
             sb.Append(SFHttpHeaderNames.Date);
             sb.Append(": ");
             sb.Append(DateTime.UtcNow.ToString("r"));
-            sb.Append("\r\n"); 
+            sb.Append("\r\n");
             sb.Append("Access-Control-Allow-Origin: *\r\n");
+            sb.Append("Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n");
             sb.Append("Access-Control-Allow-Headers: Referer, Content-Type, User-Agent\r\n");
+            sb.Append("Connection: close\r\n"); // 연결 종료 명시
+            sb.Append("Cache-Control: no-cache, no-store, must-revalidate\r\n");
+            sb.Append("Pragma: no-cache\r\n");
+            sb.Append("Expires: 0\r\n");
             if (sfHttpResponse.GetContextType() == "application/octet-stream")
             {
                 sb.Append("Content-Encoding: gzip\r\n");
